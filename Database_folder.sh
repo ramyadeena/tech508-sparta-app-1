88c9abbadbb23e6a070c116a3824857f224d66ce
Rade <ramyadayalan14@gmail.com>
2025-08-05 09:20:50 +0100

    Update Database_folder.sh " changed curl command"

--- Database_folder.sh
+++ Database_folder.sh
@@ -1,8 +1,7 @@
 
 #import  the public key
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
+ gpg --dearmor | \ sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg > /dev/null
 
 # create list file
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
@@ -25,7 +24,9 @@ sudo apt-get install -y \
 
 
 # Take a back up file for mongod.configure
-sudo cp /etc/mongod.conf/ etc/mongod.conf.bk
+
+sudo cp /etc/mongod.conf /etc/mongod.conf.bk
+
 
 # use sed command:
 sudo sed -i 's/bindIp:127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
