fa3cd555154ad8cc04e85ed44104ca8f75ef26de
Rade <ramyadayalan14@gmail.com>
2025-08-05 11:28:34 +0100

    Update prov-app.sh " included debian_frontend"

--- prov-app.sh
+++ prov-app.sh
@@ -3,17 +3,17 @@
 #provision sparta test app
 
 echo "Update ...."
-sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
 echo "Done"
 echo
 
 # Fix! expects user input
 
-sudo apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 
 # Fix! expects user input
 
-sudo apt-get install nginx -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx -y
 
 # configure reverse proxy using nginx
 
@@ -27,13 +27,13 @@ node -v
 # git clone command
 git clone https://github.com/ramyadeena/tech508-sparta-app-1.git repo
 
+ 
+# cd into app folder
+cd repo/app
 
 # set env var for connections to database
 # comment this out
 export DB_HOST=mongodb://172.31.24.151:27017/posts
- 
-# cd into app folder
-cd repo/app
 
 #Install package for the app
 npm install
